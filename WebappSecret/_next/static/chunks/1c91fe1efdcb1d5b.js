09f7bc42d292dbfe2be5d969ba28c8d8629059ec
Orçun Buğra Özel <93700345+bugraozel@users.noreply.github.com>
2025-10-15 14:20:26 +0300

    Delete WebappSecret directory

--- WebappSecret/_next/static/chunks/1c91fe1efdcb1d5b.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/2b37edf68c8b2188.js",
-  "static/chunks/9ac97421884d0897.js",
-  "static/chunks/turbopack-714d88177e0956f6.js"
-])
--- WebappSecret/_next/static/chunks/826130641ba82228.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/b2858601d506d69d.js",
-  "static/chunks/9ac97421884d0897.js",
-  "static/chunks/turbopack-e8164f8a59f6083b.js"
-])
